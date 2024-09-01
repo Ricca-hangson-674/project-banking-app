6a2b27266edb7b6e7b23b17a2622e3529b09ce3f
nrh674 <n.riccahang674@gmail.com>
2024-09-01 18:27:11 +0400

    [feat] : 01 september 2024 18:27

--- banking-app/src/main/java/com/riccahang/banking/controller/AccountController.java
+++ banking-app/src/main/java/com/riccahang/banking/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.riccahang.banking.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.riccahang.banking.dto.AccountDto;
 import com.riccahang.banking.service.AccountService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @RestController
@@ -32,4 +41,31 @@ public class AccountController {
         AccountDto accountDto = accountService.getAccountById(id);
         return ResponseEntity.ok(accountDto);
     }
+
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id, @RequestBody  Map<String, Double> request) {
+        Double amount = request.get("amount");
+        AccountDto accountDto = accountService.deposit(id, amount);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    @PutMapping("/{id}/withdraw")
+    public ResponseEntity<AccountDto> withdraw(@PathVariable Long id, @RequestBody Map<String, Double> request) {
+        Double amount = request.get("amount");
+        AccountDto accountDto = accountService.withdraw(id, amount);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountDto>> getAllAccounts() {
+        List <AccountDto> accounts = accountService.getAllAccounts();
+        return ResponseEntity.ok(accounts);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok("Account is deleted successfully !");
+    }
+    
 }
--- banking-app/src/main/java/com/riccahang/banking/service/impl/AccountServiceImpl.java
+++ banking-app/src/main/java/com/riccahang/banking/service/impl/AccountServiceImpl.java
@@ -1,5 +1,8 @@
 package com.riccahang.banking.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.riccahang.banking.dto.AccountDto;
@@ -28,6 +31,41 @@ public class AccountServiceImpl implements AccountService {
     public AccountDto getAccountById(Long id) {
         Account account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account does not exist"));
         return AccountMapper.mapToAccountDto(account);
+    }
+
+    @Override
+    public AccountDto deposit(Long id, double amount) {
+        Account account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account does not exist"));
+        double total = account.getBalance() + amount;
+        account.setBalance(total);
+        Account saveAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(saveAccount);
+    }
+
+    @Override
+    public AccountDto withdraw(Long id, double amount) {
+        Account account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account does not exist"));
+        
+        if (account.getBalance() < amount) {
+            throw new RuntimeException("Insufficient amount");
+        }
+        
+        double total = account.getBalance() - amount;
+        account.setBalance(total);
+        Account saveAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(saveAccount);
+    }
+
+    @Override
+    public List<AccountDto> getAllAccounts() {
+        List<Account> accounts = accountRepository.findAll();
+        return accounts.stream().map((account) -> AccountMapper.mapToAccountDto(account)).collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteAccount(Long id) {
+        Account account = accountRepository.findById(id).orElseThrow(() -> new RuntimeException("Account does not exist"));
+        accountRepository.deleteById(account.getId());
     }
     
 }
